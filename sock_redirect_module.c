2f324026edffb7054df9ec90dca22f383c213b52
Ryan Wang <colorfulshark@hotmail.com>
2017-12-15 18:00:16 +0800

    print sk_buff ip

--- sock_redirect_module.c
+++ sock_redirect_module.c
@@ -1,33 +1,45 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/netdevice.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
 
+#include <linux/ip.h>
+
 #include "sock_redirect_module.h"
 
-
-#define MOD_TAG "sock_redirect"
-#define LOG(msg) printk("[%s]:%s\n", MOD_TAG, msg)
-
-MODULE_LICENSE("GPL3");
+MODULE_LICENSE("GPL2");
 MODULE_AUTHOR("colorfulshark@hotmail.com");
 
 
-struct nf_hook_ops nfh_pre_op = {
-    .hook = nfh_pre_fn,
-    .owner = THIS_MODULE,
-    .pf = NFPROTO_IPV4,
-    .hooknum = NF_INET_LOCAL_OUT,
-    .priority = NF_IP_PRI_FIRST,
+struct nf_hook_ops nfh_ops[] = {
+    [0] = {
+        .hook = nfh_locout_fn,
+        .owner = THIS_MODULE,
+        .pf = NFPROTO_IPV4,
+        .hooknum = NF_INET_LOCAL_OUT,
+        .priority = NF_IP_PRI_FIRST,
+    },
 };
 
-static unsigned int nfh_pre_fn(unsigned int hooknum,
+static unsigned int nfh_locout_fn(unsigned int hooknum,
                    struct sk_buff *skb,
                    const struct net_device *in,
                    const struct net_device *out,
                    int (*okfn)(struct sk_buff *))
 {
-    LOG(__FUNCTION__);
+    u8 *sip;
+    u8 *dip;
+    struct iphdr *iph;
+    LOG("%s\n", __FUNCTION__);
+    LOG(out->name);
+    iph = ip_hdr(skb);
+    sip = (u8 *)&iph->saddr;
+    dip = (u8 *)&iph->daddr;
+    LOG("saddr: %d.%d.%d.%d\n", sip[0], sip[1], sip[2], sip[3]);
+    LOG("daddr: %d.%d.%d.%d\n", dip[0], dip[1], dip[2], dip[3]);
+
+
     return NF_ACCEPT;
 }
 
@@ -38,7 +50,7 @@ static int mod_init(void)
     int err;
     LOG("sock redirect module init\n");
     LOG("prepare to register hook op\n");
-    err = nf_register_hook(&nfh_pre_op);
+    err = nf_register_hooks(nfh_ops, ARRAY_SIZE(nfh_ops));
     if(err < 0)
         goto err_nf_reg_hook;
     return 0;
@@ -53,7 +65,7 @@ static void mod_exit(void)
 {
     LOG("sock redirect module exit\n");
     LOG("prepare to unregister hook op\n");
-    nf_unregister_hook(&nfh_pre_op);
+    nf_unregister_hooks(nfh_ops, ARRAY_SIZE(nfh_ops));
 }
 
 module_init(mod_init);
--- sock_redirect_module.h
+++ sock_redirect_module.h
@@ -1,7 +1,20 @@
 #ifndef SOCK_REDIRECT_MODULE_H
 #define SOCK_REDIRECT_MODULE_H
 
-static unsigned int nfh_pre_fn(unsigned int hooknum,
+#define debug 1
+#define MOD_TAG "sock_redirect"
+#if debug
+#define LOG(fmt, args...) \
+do{\
+    printk("[%s]:", MOD_TAG);\
+    printk(fmt, ##args);\
+}while(0)
+
+#else
+#define LOG(msg)
+#endif
+
+static unsigned int nfh_locout_fn(unsigned int hooknum,
                    struct sk_buff *skb,
                    const struct net_device *in,
                    const struct net_device *out,
